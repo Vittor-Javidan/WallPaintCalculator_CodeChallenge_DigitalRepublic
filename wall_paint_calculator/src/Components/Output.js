7e7e65d2cfb4a92cd286336739f67247d49c0814
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-04-23 06:12:24 -0300

    Outuput.js: now feedbackUpdate() is triggered by a button

--- wall_paint_calculator/src/Components/Output.js
+++ wall_paint_calculator/src/Components/Output.js
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useState } from 'react'
 import { AppContext } from '../App'
 
 import feedbackUpdate from './Scripts/feedBackUpdate'
@@ -8,7 +8,7 @@ export default function Output() {
     const { walls } = useContext(AppContext)
 
     //feedback states
-    const [status, setStatus] = useState(`Waiting for input data`)
+    const [status, setStatus] = useState('Click "Refresh" to calculate')
     const [totalWallArea, setTotalWallArea] = useState(0)
     const [totalDoorArea, setTotalDoorArea] = useState(0)
     const [totalWindowArea, setTotalWindowArea] = useState(0)
@@ -23,11 +23,6 @@ export default function Output() {
         neededCans      , setTotalCans
     }
 
-    useEffect(() => {
-        feedbackUpdate(props)
-    }, [walls])
-
-
     return (
         <div className='Output-div'>
             <h2>Status: {status}</h2>
@@ -35,6 +30,12 @@ export default function Output() {
             <h2>total Door Area: {totalDoorArea}</h2>
             <h2>total Window Area: {totalWindowArea}</h2>
             <h2>total Cans: {neededCans}</h2>
+            <button 
+                className='Output-button'
+                onClick={()=>{feedbackUpdate(props)}}
+            >
+                Refresh
+            </button>
         </div>
     )
 }
