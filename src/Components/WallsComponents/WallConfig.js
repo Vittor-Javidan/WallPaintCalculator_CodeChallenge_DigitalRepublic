0829a905c030dc5e10991621b9acd2f323005ccc
Vittor Javidan <vittor.javidan.t@gmail.com>
2022-04-24 10:02:34 -0300

    Minor adjustments on css and html structure

--- src/Components/WallsComponents/WallConfig.js
+++ src/Components/WallsComponents/WallConfig.js
@@ -8,7 +8,9 @@ export default function WallConfig({ index }) {
 
     return (
         <div className="WallConfig-div">
-            <h2 className="WallConfig-label">{`Wall ${index + 1} Configuration: `}</h2>
+            <h2 className="WallConfig-label">
+                {`Wall ${index + 1} Configuration: `}
+            </h2>
             <WidthInput index={index} />
             <ObjectsAmountInput index={index} />
             <ObjectsArray index={index} />
--- src/Components/WallsComponents/WallConfigComponents/ObjectConfig.js
+++ src/Components/WallsComponents/WallConfigComponents/ObjectConfig.js
@@ -6,7 +6,9 @@ export default function ObjectConfig({index_i, index_j}){
 
     return (
         <div className="ObjectConfig-div">
-            <label className="ObjectConfig-label">{`Object ${index_j + 1} Configuration: `}</label>
+            <h3 className="ObjectConfig-label">
+                {`Object ${index_j + 1} Configuration: `}
+            </h3>
             <ObjectNameInput index_i={index_i} index_j={index_j} />
             <ObjectWidthInput index_i={index_i} index_j={index_j} />
             <ObjectHeightInput index_i={index_i} index_j={index_j} />
